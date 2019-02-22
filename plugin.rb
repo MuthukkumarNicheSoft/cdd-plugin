d6b1dc4dbce64807d15a8dff9f285e4be2ef8a68
Muthukkumar <muthukkumar@nichesoft.net>
2019-02-22 12:18:02 +0530

    removed .git extension

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 # about: CSS theme
 # version: 0.1
 # authors: Muthukkumar
-# url: https://github.com/MuthukkumarNicheSoft/discourse-theme-amnimo.git
+# url: https://github.com/MuthukkumarNicheSoft/discourse-theme-amnimo
 
 register_asset 'stylesheets/common.scss'
 
